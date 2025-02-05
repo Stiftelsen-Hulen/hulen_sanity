ec13321bcad09e6a13c7663b5c3a33a3c994813b
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2025-02-05 19:36:02 +0100

    added form title field

--- schemas/joinUsPage/joinEmailForm.ts
+++ schemas/joinUsPage/joinEmailForm.ts
@@ -7,6 +7,11 @@ export default {
   fieldsets: [{ name: 'joinFormLabels', title: 'Email Form\'s Input Labels' }],
   fields: [
     {
+      type: 'string',
+      name: 'emailFormTitle',
+      validation: (rule: Rule) => rule.required(),
+    },
+    {
       type: 'localeString',
       name: 'emailFormLabel',
       validation: (rule: Rule) => rule.required(),
